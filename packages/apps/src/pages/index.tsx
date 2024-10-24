b4c0f63d0aa15e2b97b527c29cff354e866dd115
SeongOh Park <qkrtjddh1212@naver.com>
2024-10-24 13:16:41 +0900

    ✨ restapi 배포 changes

--- packages/apps/src/pages/index.tsx
+++ packages/apps/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        ㄴㅇㄹwfewfwefwfwefeewfㅠㅠㅠ
+        이번엔 apps만 할겁니다~ ㄴㅇㄹwfewfwefwfwefeewfㅠㅠㅠ
         <div className={styles.description}>
           이건 되겠지?wefwefefwfwefewf
           <p>
