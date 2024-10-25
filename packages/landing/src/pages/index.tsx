d90f1b37578fb5b685350b7d007e0d428750214c
SeongOh Park <qkrtjddh1212@naver.com>
2024-10-25 15:11:10 +0900

    ✨ landing 배포

--- packages/landing/src/pages/index.tsx
+++ packages/landing/src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          요기두!!
+          요기두!!sdfdsfds
           <p>
             I want to Deploy to Landing&nbsp;qwewqe
             <code className={styles.code}>src/pages/index.tsx</code>
