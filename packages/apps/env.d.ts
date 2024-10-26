b599d72f91aab9088a57b8e64039e07340a4f4c3
SeongOh Park <qkrtjddh1212@naver.com>
2024-10-26 18:44:23 +0900

    ✨ apps env 출력

---
+++ packages/apps/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_API_KEY: string;
+    MY_ENV: string;
+    MORE_ENV: string;
+    LOCALE_ENV: string;
+    APPS_ENV: string;
+  }
+}
