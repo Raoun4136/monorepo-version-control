e2fc48434308fb0a80baf0eee426f429b8e802ee
SeongOh Park <qkrtjddh1212@naver.com>
2024-10-26 16:25:32 +0900

    🎨 병합확인 3

--- packages/landing/src/pages/index.tsx
+++ packages/landing/src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              wefwefew By{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
