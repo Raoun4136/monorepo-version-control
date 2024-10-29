ce8ecb3259b8551a5c3825069e34b855a05f4f42
SeongOh Park <qkrtjddh1212@naver.com>
2024-10-29 11:33:57 +0900

    ✨ node env에 따른 vercel toolbar 설치

--- packages/apps/src/pages/_app.tsx
+++ packages/apps/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { VercelToolbar } from '@vercel/toolbar/next';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const showToolbar =
+    process.env.NEXT_PUBLIC_NODE_ENV === 'development' ||
+    process.env.NEXT_PUBLIC_NODE_ENV === 'staging';
+
+  return (
+    <>
+      <Component {...pageProps} />
+      {showToolbar && <VercelToolbar />}
+    </>
+  );
 }
